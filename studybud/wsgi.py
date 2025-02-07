5114dc8c89369a55ea4b2e80db2475181a1c58c9
aamidshah <shahaamid33@gmail.com>
2025-02-07 14:18:39 +0530

    Fixed Vercel deployment, updated settings, and static files

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for studybud project.
+# """
+# WSGI config for studybud project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+# """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
+
+# application = get_wsgi_application()
+# app = application  # Required for Vercel
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()  # Vercel requires "app", not just "application"
